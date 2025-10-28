4b5910c6fe005ef50728658a6504e3c3871f0ee2
lysrain21 <liyushen24@mails.ucas.ac.cn>
2025-10-28 11:00:02 +0800

    change block-number

--- my-token-ethereum/src/usdc.ts
+++ my-token-ethereum/src/usdc.ts
@@ -1,8 +1,9 @@
 import {
   AdminChanged as AdminChangedEvent,
-  Upgraded as UpgradedEvent
+  Upgraded as UpgradedEvent,
+  Transfer as TransferEvent
 } from "../generated/USDC/USDC"
-import { AdminChanged, Upgraded } from "../generated/schema"
+import { AdminChanged, Upgraded, Transfer } from "../generated/schema"
 
 export function handleAdminChanged(event: AdminChangedEvent): void {
   let entity = new AdminChanged(
@@ -23,6 +24,21 @@ export function handleUpgraded(event: UpgradedEvent): void {
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
   entity.implementation = event.params.implementation
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTransfer(event: TransferEvent): void {
+  let entity = new Transfer(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.value = event.params.value
 
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
